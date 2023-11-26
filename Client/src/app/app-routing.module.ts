3f3066837e83639c22749ff4827803acaf09e375
malathyjeyram <malathyjeyram@gmail.com>
2023-11-26 17:43:33 +0530

    routing added to the login page in angular

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'signin', component: SignInComponent
+  },
+  {
+    path:' ', component:SignInComponent,pathMatch:'full'
+  },
+  {
+    path:'**', component:SignInComponent,pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
